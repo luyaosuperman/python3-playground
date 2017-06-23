a5cd2bfc9aa6423718b6529a4296a2c96da81963
luyao <luyaosuperman@gmail.com>
2017-06-23 22:10:40 +1000

    commit

--- homework/flownet/graph.py
+++ homework/flownet/graph.py
@@ -1,29 +1,99 @@
+import heapq
+import math
+from xprint import xPrint, printStack
+
 class UndirectionalGraph():
     """
     Graph itself composed of nodes and edges
-    input will be a text file
-    15          : Number of nodes
-    0 1 3       : node1, node2, and the weight of the edge
-
-
-
     """
 
-    def __init__(self):
+    def __init__(self, numberOfNodes):
         """
         Init the graph
         """
-        self.nodes = [] # list of nodes in the graph
+        # assume nodes start from 0, stop at numberOfNodes - 1
+        self.numberOfNodes = numberOfNodes 
+        
+        # record the edge and its weight
+        # every edge is recorded twice
+        # {node1: {node2:1, node3:2}, ...}
+        self.edges = dict() 
 
+        # record the frequency of edge being used
+        # only record once per edge
+        # {(smallerNode, largerNode):count, ...}
+        self.edgesUsed = dict() 
 
-    def addNode(self, node):
-        """
-        add nodes to the graph
-        """
-        self.nodes.append(node)
-
-    def addEdge()
+    def addEdge(self, node1, node2, weight):
         """
         add edges to the graph
         """
-        pass
+        if node1 not in self.edges:
+            self.edges[node1] = dict()
+
+        if node2 not in self.edges:
+            self.edges[node2] = dict()
+
+        self.edges[node1][node2] = weight
+        self.edges[node2][node1] = weight
+        xPrint(self.edges)
+
+
+    def dijkstra(self, node):
+        """
+        Calculate the shortest spannning tree from a node
+        """
+        #nodesVisisted = [False] * self.numberOfNodes
+        nodesDistance = [math.inf] * self.numberOfNodes
+        nodesPrevious = [-1] * self.numberOfNodes
+
+        nodesDistance[node] = 0
+
+        minpq = []
+        for node in range(self.numberOfNodes):
+            heapq.heappush(minpq, [nodesDistance[node],node]) # value of minpq, item
+
+        while len(minpq) > 0:
+            item = heapq.heappop(minpq)
+            [currentDist, currentNode] = item
+            for neighborNode in self.edges[currentNode]:
+                distanceToNeighborNode = self.edges[currentNode][neighborNode]
+                distance = nodesDistance[currentNode] + distanceToNeighborNode
+                if distance < nodesDistance[neighborNode]:
+                    nodesDistance[neighborNode] = distance
+                    nodesPrevious[neighborNode] = currentNode
+                    # decrease priority of the item
+                    item[0] = distance
+                    heapq.heapify(minpq)
+        xPrint(nodesDistance)
+        xPrint(nodesPrevious)
+
+if __name__ == "__main__":
+    graph = UndirectionalGraph(3)
+    graph.addEdge(0,1,1)
+    graph.addEdge(0,2,4)
+    graph.addEdge(1,2,2)
+    graph.dijkstra(0)
+
+
+    graph = UndirectionalGraph(6)
+    graph.addEdge(0,1,2)
+    graph.addEdge(1,2,3)
+    graph.addEdge(0,2,1)
+    graph.addEdge(2,3,5)
+    graph.addEdge(3,4,5)
+    graph.addEdge(3,5,7)
+    graph.addEdge(4,5,3)
+    graph.dijkstra(0)
+
+
+
+
+
+        
+
+
+
+
+
+
---
+++ homework/flownet/xprint.py
@@ -0,0 +1,19 @@
+debugPrint = True # only print if it is True
+stackPrint = False
+def xPrint(*args):
+	"""
+	A print function that can be turned on/off
+	"""
+	if debugPrint:
+		print(*args)
+
+def printStack(method):
+	"""
+	print the name of the method
+	"""
+	def printed(*args, **kw):
+		if stackPrint:
+			xPrint("****", method.__name__)
+		result = method(*args, **kw)
+		return result
+	return printed
